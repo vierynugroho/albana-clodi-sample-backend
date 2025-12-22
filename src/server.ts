4804a530f5ee775ae048b9384e766f128723e979
vnonymous_ <viery15102002@gmail.com>
2025-12-22 08:36:00 +0700

    fix: disabled cors

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ app.set("trust proxy", true);
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 app.use(compression());
 
